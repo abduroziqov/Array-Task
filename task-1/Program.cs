7387a00afec06db28d6ee69e2cc816fd29a28d2a
QUVONCHBEK ABDUROZIQOV <75139080+abduroziqov@users.noreply.github.com>
2023-04-08 15:55:12 +0500

    true || false

---
+++ task-1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 0;
+            GetNumber(ref row);
+            FillArray(row);
+        }
+        public static void GetNumber(ref int row)
+        {
+            Console.Write("Enter row value=> ");
+            row = int.Parse(Console.ReadLine());
+        }
+        public static void FillArray(int row)
+        {
+            Random random = new Random();
+            int[] array = new int[row];
+
+            for (int i = 0; i < row; i++)
+            {
+
+                array[i] = random.Next(1, 10);
+                Console.Write($"[ {array[i]} ] ");
+
+            }
+            Console.WriteLine();
+
+            if (array[0] == 6 || array[row - 1] == 6)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }              
+        }
+    }
+}
