2d47c37faa41f3fb2564894614757682fae081fd
QUVONCHBEK ABDUROZIQOV <75139080+abduroziqov@users.noreply.github.com>
2023-04-09 16:43:34 +0500

    reverse array

---
+++ task_4/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 0;
+            GetNumber(ref row);
+            FillArray(row);
+        }
+        public static int GetNumber(ref int row)
+        {
+            Console.Write("Enter row length: ");
+            row = int.Parse(Console.ReadLine());
+            return row;
+        }
+        public static int FillArray(int row)
+        {
+            Random random = new Random();
+            int[] array = new int[row];
+
+            for (int i = 0; i < row; i++)
+            {
+                array[i] = random.Next(1, 10);
+                Console.Write($"[ {array[i]} ] ");
+            }
+            Console.WriteLine();
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"[ {array[i]} ] ");
+            }
+            return row;
+        }
+    }
+}
